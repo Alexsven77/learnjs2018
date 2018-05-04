7f651ff1f9051a0a731b4b064870a94126c57d9a
Alex Sven <mr.alexsven@mail.ru>
2018-05-04 19:02:46 +0300

    6

--- src/index.js
+++ src/index.js
@@ -30,8 +30,7 @@ function returnFirstArgument(a) {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
-    b=b || 100;
+function sumWithDefaults(a, b=100) {
     return a+b;
 }
 
@@ -63,8 +62,7 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(a) {
-  a = a || 0;
+function returnCounter(a=0) {
     return  function r(){
        return ++a;
     }
@@ -80,11 +78,7 @@ function returnCounter(a) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
-  var arr = [];
-  for (var i=0 ; i < arguments.length; i++ ){
-    arr.push(arguments[i]);
-  }
-  return arr;
+  return [...arguments];
 }
 
 /*
@@ -102,8 +96,12 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+function bindFunction(fn, ...arg) {
+  return () => {
+return fn.apply(null,arg);
+  };
 }
+
 
 export {
     returnFirstArgument,
