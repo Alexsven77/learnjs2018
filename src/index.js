08a08c5d3592dc3945c357a566066dd61a7ded32
Alex Sven <mr.alexsven@mail.ru>
2018-05-31 22:29:06 +0300

    dz4 common

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+  let div = document.createElement('div');
+  div.innerHTML = `<p>${text}</p>`;
+  return div;
 }
 
 /*
@@ -22,6 +25,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
+  where.insertBefore(what, where.firstChild);
 }
 
 /*
@@ -44,6 +48,12 @@ function prepend(what, where) {
    findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
+  let arr =[];
+  let p = where.getElementsByTagName('p');
+  for (let child of p){
+    arr.push(child.previousElementSibling);
+  }
+  return arr;
 }
 
 /*
@@ -66,7 +76,7 @@ function findAllPSiblings(where) {
 function findError(where) {
     var result = [];
 
-    for (var child of where.childNodes) {
+    for (var child of where.children) {
         result.push(child.innerText);
     }
 
@@ -86,6 +96,12 @@ function findError(where) {
    должно быть преобразовано в <div></div><p></p>
  */
 function deleteTextNodes(where) {
+
+    for (var child of where.childNodes) {
+       if (child.nodeType === 3) 
+       where.removeChild(child);
+    }
+
 }
 
 /*
@@ -93,15 +109,28 @@ function deleteTextNodes(where) {
 
  Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента (углубляться в дерево)
 
- Задачу необходимо решить без использования рекурсии, то есть можно не уходить вглубь дерева.
- Так же будьте внимательны при удалении узлов, т.к. можно получить неожиданное поведение при переборе узлов
-
  Пример:
    После выполнения функции, дерево <span> <div> <b>привет</b> </div> <p>loftchool</p> !!!</span>
    должно быть преобразовано в <span><div><b></b></div><p></p></span>
  */
-function deleteTextNodesRecursive(where) {
-}
+/*function deleteTextNodesRecursive(where) {
+  for (var child of where.childNodes) {
+      if (child.nodeType === 3) 
+      where.removeChild(child);
+      else{
+      //child.innerText='';
+      console.log(child);
+      deleteTextNodesRecursive(child);
+      }
+    }
+  }*/
+
+  function deleteTextNodesRecursive(where) {
+    for (var i = 0; i < where.childNodes.length; i++)
+      where.childNodes[i].nodeType === 3 ? where.childNodes[i].data = '' : deleteTextNodesRecursive(where.childNodes[i]);
+      
+} 
+
 
 /*
  Задание 7 *:
